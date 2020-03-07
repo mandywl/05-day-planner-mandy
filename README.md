6bb14b98a0d00262360afb671421d54722a41769
AU\mandy.wells-lakeland <mandy.wellslakeland@akqa.com>
2020-03-07 22:48:37 +1100

    Added Readme and completed scheduler functions

--- README.md
+++ README.md
@@ -1,46 +1,28 @@
 # 05 Third-Party APIs: Work Day Scheduler
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+# Description 
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+* This is a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
-## User Story
 
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
+# App Screenshot 
+![App Screenshot](appScreenShot.png)
 
-## Acceptance Criteria
 
-```
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
+# Deployed url
 
-The following animation demonstrates the application functionality:
+* https://github.com/mandywl/04-web-apis-code-quiz-mandy
+* https://mandywl.github.io/04-web-apis-code-quiz-mandy/
 
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
 
-## Review
+# Instructions
 
-You are required to submit the following for review:
+* The application displays timeblocks for standard business hours (9 a.m. to 5 p.m.)
 
-* The URL of the deployed application.
+* Each timeblock contains an input field and save button
 
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+* Clicking a time block's save button saves the input text to local storage, allowing the text to persist when the application is refreshed
 
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+* The current day is displayed at the top of the calendar
+
+* Each timeblock is color coded to indicate whether it is in a past, present, or future hour
--- script.js
+++ script.js
@@ -9,6 +9,10 @@ var year = date.getFullYear();
 var dayname = date.getDay();
 var ampm = hours >= 12 ? 'pm' : 'am';
 var childElement = 0;
+var comment;
+var valueEntered;
+var yesterday;
+var today;
 
 var monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
@@ -18,43 +22,67 @@ function getOrdinalNum(n) {
     return n + (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
 }
 
-function displayTimeTable(time) {
+function displayTimeTable(time, ele) {
     $(".container").append(
         $('<div/>', { 'class': 'row' }).append(
             $('<div/>', { 'class': 'col-2 hour' }).append($(
-                '<span/>', { 'class': '', text: i + ":00" }))
-        ).append($('<textarea/>', { 'class': 'col-7', 'id': 'text' })).append($('<div/>', { 'class': 'col-2 saveBtn' }).append($(
-            '<span/>', { 'class': 'fa fa-save' }))));
+                '<span/>', { 'class': '', text: time + ":00" }))
+        ).append($('<textarea/>', { 'class': 'col-7', 'id': ele })).append($('<div/>', { 'class': 'col-2 saveBtn' }).append($(
+            '<i/>', { 'class': 'fa fa-save' }))));
 }
 
 $("#currentDay").text(week[dayname] + " " + monthNames[month] + ", " + getOrdinalNum(day));
 
-for (var i = 9; i < 22; i++) {
-    // hours = hours % 12;
-    // hours = hours ? hours : 12; // the hour '0' should be '12'
-    displayTimeTable(i)
-    if (i < hours) {
-        console.log("i is ", i);
-        console.log("hours is ", hours)
-        $(".col-7").eq(childElement).addClass("past");
-    } else if (i === hours) {
-        $(".col-7").eq(childElement).addClass("present");
-    } else {
-        $(".col-7").eq(childElement).addClass("future");
-    }
-    childElement++;
-}
+
 
 
 $(document).ready(function () {
-    $('.saveBtn').on('click', function (event) {
-        var comment = $('textarea#text').val();
-        console.log("clicked on ", $(this).closest().val());
-        console.log("comment is ", comment)
-        if ($('textarea#text') != undefined) {
-            var message = $('textarea#text').val();
-            console.log("message is ", message)
+    var elementClicked;
+    for (var i = 9; i < 22; i++) {
+        // hours = hours % 12;
+        // hours = hours ? hours : 12; // the hour '0' should be '12'
+        displayTimeTable(i, childElement)
+        if (i < hours) {
+            // console.log("i is ", i);
+            // console.log("hours is ", hours)
+            $(".col-7").eq(childElement).addClass("past");
+        } else if (i === hours) {
+            $(".col-7").eq(childElement).addClass("present");
+        } else {
+            $(".col-7").eq(childElement).addClass("future");
         }
-        localStorage.setItem("comment", JSON.stringify(comment));
+        //$(".col-7").eq(childElement).attr("data-name", childElement);
+        childElement++;
+    }
+
+    $('.saveBtn').on('click', function (event) {
+        event.preventDefault()
+        event.stopPropagation();
+        const value = $(this).siblings("textarea").val();
+        const id = $(this).siblings("textarea").attr("id")
+        //console.log(value, id)
+        saveValue({ id, value })
+        elementClicked = $(this).prev().attr("data-name", "clicked");
+        $(this).data('clicked', true);
     });
+
+    for (var i = 0; i < 12; i++) {
+        document.getElementById(i).value = getSavedValue(i);
+
+    }
+
 });
+
+function saveValue(e) {
+    var id = e.id;  // get the sender's id to save it . 
+    var val = e.value; // get the value. 
+    localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override . 
+}
+
+//get the saved value function - return the value of "v" from localStorage. 
+function getSavedValue(v) {
+    if (!localStorage.getItem(v)) {
+        return "";
+    }
+    return localStorage.getItem(v);
+}
